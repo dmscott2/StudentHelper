7347d590ea26bfd26744701f65e36d2df664963a
Dawn Scott <dmscott2@buffs.wtamu.edu>
2021-12-07 22:35:23 -0600

    UpdatedCommit

--- Pages/Courses/Create.cshtml.cs
+++ Pages/Courses/Create.cshtml.cs
@@ -1,174 +1,44 @@
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using StudentHelper.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using StudentHelper.Models;
 
 namespace StudentHelper.Pages.Courses
 {
-	public class CreateModel : PageModel
-	{
-		private readonly IConfiguration _configuration;
-		private string _connectionString;
-		DbContextOptionsBuilder<StudentHelperDbContext> _optionsBuilder;
+    public class CreateModel : PageModel
+    {
+        private readonly StudentHelper.Models.StudentHelperDbContext _context;
 
-		public CourseController(IConfiguration configuration)
-		{
-			_configuration = configuration;
-			_optionsBuilder = new DbContextOptionsBuilder<StudentHelperDbContext>();
-			_connectionString = _configuration.GetConnectionString("DefaultConnection");
-			_optionsBuilder.UseSqlServer(_connectionString);
-		}
-		// GET: CourseController
-		public ActionResult Index()
-		{
-			try
-			{
-				using (StudentHelperDbContext _context = new StudentHelperDbContext(_optionsBuilder.Options))
-				{
+        public CreateModel(StudentHelper.Models.StudentHelperDbContext context)
+        {
+            _context = context;
+        }
 
-					var courses = _context.Courses.ToList();
-					return View(courses);
-				}
-			}
-			catch (Exception ex)
-			{
-				throw ex;
-			}
-		}
+        public IActionResult OnGet()
+        {
+        ViewData["CourseId"] = new SelectList(_context.Courses, "CourseID", "CourseName");
+            return Page();
+        }
 
-		// GET: CourseController/Details/5
-		public ActionResult Details(int id)
-		{
-			using (StudentHelperDbContext _context = new StudentHelperDbContext(_optionsBuilder.Options))
-			{
+        [BindProperty]
+        public Course Course { get; set; }
 
-				var course = _context.Courses.FirstOrDefault(s => s.CourseID == id);
-				return View(course);
-			}
-		}
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
 
-		// GET: CourseController/Create
-		public ActionResult Create()
-		{
-			//using (StudentHelperDbContext _context = new StudentHelperDbContext(_optionsBuilder.Options))
-			//{
+            _context.Courses.Add(Course);
+            await _context.SaveChangesAsync();
 
-			//	var students = _context.Students.ToList();
-			//	ViewData["Students"] = students;
-			//}
-
-			return View();
-		}
-
-		// POST: CourseController/Create
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Create(Course course)
-		{
-			try
-			{
-
-				using (StudentHelperDbContext _context = new StudentHelperDbContext(_optionsBuilder.Options))
-				{
-
-					var students = _context.Courses.Add(course);
-					_context.SaveChanges();
-				}
-
-				return RedirectToAction(nameof(Index));
-			}
-			catch (Exception ex)
-			{
-				return View();
-			}
-		}
-
-		// GET: CourseController/Edit/5
-		public ActionResult Edit(int id)
-		{
-			try
-			{
-				using (StudentHelperDbContext _context = new StudentHelperDbContext(_optionsBuilder.Options))
-				{
-
-					var course = _context.Courses.FirstOrDefault(s => s.CourseID == id);
-					return View(course);
-				}
-
-			}
-			catch (Exception)
-			{
-
-				throw;
-			}
-		}
-
-		// POST: CourseController/Edit/5
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Edit(int id, Course course)
-		{
-			try
-			{
-				using (StudentHelperDbContext _context = new StudentHelperDbContext(_optionsBuilder.Options))
-				{
-					_context.Courses.Update(course);
-					_context.SaveChanges();
-				}
-
-				return RedirectToAction(nameof(Index));
-			}
-			catch
-			{
-				return View();
-			}
-		}
-
-		// GET: CourseController/Delete/5
-		public ActionResult Delete(int id)
-		{
-			try
-			{
-				using (StudentHelperDbContext _context = new StudentHelperDbContext(_optionsBuilder.Options))
-				{
-
-					var course = _context.Courses.FirstOrDefault(s => s.CourseID == id);
-					return View(course);
-				}
-
-			}
-			catch (Exception)
-			{
-
-				throw;
-			}
-		}
-
-		// POST: CourseController/Delete/5
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Delete(int id, Course course)
-		{
-			try
-			{
-				using (StudentHelperDbContext _context = new StudentHelperDbContext(_optionsBuilder.Options))
-				{
-					course.CourseID = id;
-					_context.Courses.Remove(course);
-					_context.SaveChanges();
-				}
-
-				return RedirectToAction(nameof(Index));
-			}
-			catch
-			{
-				return View();
-			}
-		}
-	}
+            return RedirectToPage("./Index");
+        }
+    }
 }
