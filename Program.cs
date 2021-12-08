2212d5b48945229c820361075bf907da70179718
Dawn Scott <dmscott2@buffs.wtamu.edu>
2021-12-07 20:02:43 -0600

    Updated Commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace StudentHelper.Models
 {
